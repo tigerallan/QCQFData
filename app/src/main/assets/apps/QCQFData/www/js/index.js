d198f8db9448f1c78db1315bfdf96e92d0e96cc6
tigerallan <allanzdl@gmail.com>
2018-01-18 20:36:35 +0800

    tigerallan. changes H5 pages.

--- app/src/main/assets/apps/QCQFData/www/js/index.js
+++ app/src/main/assets/apps/QCQFData/www/js/index.js
@@ -26,6 +26,19 @@ function table() {
 	}
 }
 
+var immersed = 0;
+var ms=(/Html5Plus\/.+\s\(.*(Immersed\/(\d+\.?\d*).*)\)/gi).exec(navigator.userAgent);
+if(ms&&ms.length>=3){ // 当前环境为沉浸式状态栏模式
+  immersed=parseFloat(ms[2]);// 获取状态栏的高度
+}
+
+var t=document.getElementById('header'),
+  c=document.getElementById('content');
+t&&(t.style.paddingTop=immersed+'px', t.style.height=t.offsetHeight+immersed+'px');
+c&&(c.style.paddingTop=parseInt(window.getComputedStyle(c)['padding-top'])+immersed+'px');
+
+alert(JSON.stringify(t.offsetHeight));
+
 function getUrlData(url) {
 	var returnData = null;
 	if(url.indexOf('?') !== -1) {
